69d5e939303028863d5cbf2d0ff57f79b39e3303
DavidLejeune <Lejeune.David@outlook.com>
2019-03-15 21:48:39 +0100

    correct path in sub batch

--- dale-terminal-4-remco/copy_and_execute_dale.bat
+++ dale-terminal-4-remco/copy_and_execute_dale.bat
@@ -1,5 +1,5 @@
 echo "COPYING DALE TERMINAL 4 REMCO TO USER HARD DRIVE"
-robocopy /R:1 /E /W:1 ./dale-terminal-4-remco %UserProfile%/dale-terminal-4-remco/
+robocopy /R:1 /E /W:1 ../dale-terminal-4-remco %UserProfile%/dale-terminal-4-remco/
 echo "EXECUTING THE SCRIPT"
 powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -file %UserProfile%/dale-terminal-4-remco/dale-terminal.ps1' -verb RunAs}"
 
